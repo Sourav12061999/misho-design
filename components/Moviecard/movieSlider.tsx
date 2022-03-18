47e209c90838337b86c6e3beea50ca4ef1db0aca
Sourav Das <sdunity12061999@gmail.com>
2022-03-18 12:16:11 +0530

    made the slider dynamic

--- components/Moviecard/movieSlider.tsx
+++ components/Moviecard/movieSlider.tsx
@@ -3,23 +3,41 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
 import MovieCard from "./movieCard";
-export default function MovieSlider() {
+import { Typography } from "@mui/material";
+
+interface Data {
+  movieName: string;
+  image: string;
+  about: string;
+  rating: number;
+}
+interface Props {
+  heading: string;
+  data: [Data];
+}
+export default function MovieSlider(props: Props) {
+  const { heading, data } = props;
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={2}>
-        <Grid item sm={6} xs={12} md={4} lg={3}>
-          <MovieCard />
+    <Box>
+      <Box sx={{ flexGrow: 1 }}>
+        <Typography variant="h4" gutterBottom>
+          {heading}
+        </Typography>
+        <Grid container spacing={2}>
+          <Grid item sm={6} xs={12} md={4} lg={3}>
+            <MovieCard />
+          </Grid>
+          <Grid item sm={6} xs={12} md={4} lg={3}>
+            <MovieCard />
+          </Grid>
+          <Grid item sm={6} xs={12} md={4} lg={3}>
+            <MovieCard />
+          </Grid>
+          <Grid item sm={6} xs={12} md={4} lg={3}>
+            <MovieCard />
+          </Grid>
         </Grid>
-        <Grid item sm={6} xs={12} md={4} lg={3}>
-          <MovieCard />
-        </Grid>
-        <Grid item sm={6} xs={12} md={4} lg={3}>
-          <MovieCard />
-        </Grid>
-        <Grid item sm={6} xs={12} md={4} lg={3}>
-          <MovieCard />
-        </Grid>
-      </Grid>
+      </Box>
     </Box>
   );
 }
