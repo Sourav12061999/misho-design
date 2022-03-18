95db0d45d1c5c4aa62866e00b81f0d950d3742f1
Sourav Das <sdunity12061999@gmail.com>
2022-03-18 15:09:02 +0530

    coresal added

--- components/Moviecard/movieSlider.tsx
+++ components/Moviecard/movieSlider.tsx
@@ -1,10 +1,9 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-
+import Carousel from "react-material-ui-carousel";
 import MovieCard from "./movieCard";
 import { Typography } from "@mui/material";
-
 interface Data {
   id: number;
   heading: string;
@@ -22,16 +21,23 @@ export default function MovieSlider(props: Props) {
   return (
     <Box>
       <Box sx={{ flexGrow: 1 }}>
-        <Typography variant="h4" gutterBottom>
+        <Typography variant="h4" color={"white"} gutterBottom>
           {heading}
         </Typography>
-        <Grid container spacing={2}>
+        {/* <Grid container spacing={2}>
           {data.map((element: Data) => (
             <Grid item key={element.id} sm={6} xs={12} md={4} lg={3}>
               <MovieCard data={element} />
             </Grid>
           ))}
-        </Grid>
+        </Grid> */}
+        <Carousel swipe animation="slide" indicators={false}>
+          {data.map((element: Data) => (
+            <React.Fragment key={element.id}>
+              <MovieCard data={element} />
+            </React.Fragment>
+          ))}
+        </Carousel>
       </Box>
     </Box>
   );
